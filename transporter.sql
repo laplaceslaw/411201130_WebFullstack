b8e090c7fc2f72a3a5b577fca491e60fb9a2b486
indra <411201130@mahasiswa.undira.ac.id>
2023-07-19 11:52:13 +0700

    Upload Database SQL

--- transporter.sql
+++ transporter.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2023 at 02:33 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.22
+-- Generation Time: Jul 19, 2023 at 06:51 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `transporter266`
+-- Database: `webfullstack`
 --
 
 -- --------------------------------------------------------
@@ -34,15 +34,16 @@ CREATE TABLE `barang` (
   `deskripsi` text DEFAULT NULL,
   `stok_barang` int(11) DEFAULT NULL,
   `harga_barang` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `deskripsi`, `stok_barang`, `harga_barang`) VALUES
-(1, '1', 'Susu Ultra', 'Susu Ultra memiliki kandungan gizi yang baik dan lengkap', 200, 6000),
-(2, '2', 'Indomie', 'Indomie adalah merek mi instan yang diproduksi oleh Indofood CBP, anak perusahaan Indofood Sukses Makmur di Indonesia', 30, 3000);
+(1, '1', 'Fox', 'Permen', 50, 2000),
+(2, '2', 'Yupi', 'Permen', 50, 1500),
+(3, '3', 'Sugus', 'Permen', 50, 1000);
 
 -- --------------------------------------------------------
 
@@ -55,14 +56,14 @@ CREATE TABLE `kurir` (
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `kurir`
 --
 
 INSERT INTO `kurir` (`id`, `name`, `email`, `password`) VALUES
-(1, 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3');
+(1, 'indra', 'indra@indra.com', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -74,16 +75,16 @@ CREATE TABLE `lokasi` (
   `id` int(11) NOT NULL,
   `kode_lokasi` varchar(10) DEFAULT NULL,
   `nama_lokasi` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `lokasi`
 --
 
 INSERT INTO `lokasi` (`id`, `kode_lokasi`, `nama_lokasi`) VALUES
-(1, '1', 'Aceh'),
-(2, '2', 'Kalimantan Barat'),
-(3, '3', 'Jogja');
+(1, '1', 'Bandung'),
+(2, '2', 'Jakarta'),
+(3, '3', 'Makasar');
 
 -- --------------------------------------------------------
 
@@ -100,17 +101,18 @@ CREATE TABLE `pengiriman` (
   `jumlah_barang` int(11) NOT NULL,
   `harga_barang` int(11) DEFAULT NULL,
   `kurir_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `pengiriman`
 --
 
 INSERT INTO `pengiriman` (`id`, `no_pengiriman`, `tanggal`, `lokasi_id`, `barang_id`, `jumlah_barang`, `harga_barang`, `kurir_id`) VALUES
-(12, '1', '2023-07-15', 3, 2, 120, 360000, 1),
-(13, '2', '2023-07-15', 1, 1, 10, 60000, 1),
-(14, '3', '2023-07-15', 3, 2, 20, 60000, 1),
-(15, '4', '2023-07-15', 3, 1, 40, 240000, 1);
+(1, 'UAS001', '2023-07-18', 3, 2, 70, 105000, 1),
+(2, 'UAS002', '2023-07-18', 1, 1, 50, 100000, 1),
+(3, 'UAS003', '2023-07-19', 3, 2, 30, 45000, 1),
+(4, 'UAS004', '2023-07-19', 3, 1, 40, 80000, 1),
+(5, 'UAS005', '2023-07-20', 2, 3, 60, 60000, 1);
 
 --
 -- Indexes for dumped tables
@@ -148,7 +150,7 @@ ALTER TABLE `pengiriman`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `kurir`
